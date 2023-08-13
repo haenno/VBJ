436c6e0798d3223dcefef1099be08213b1b093ae
Henning 'haenno' Beier <haenno@web.de>
2023-08-13 22:06:29 +0200

    add notfound view

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "/views/HomeView.vue";
 import ExampleApiCallView from "/views/ExampleApiCallView.vue";
 import LoginView from "/views/LoginView.vue";
+import NotFoundView from "/views/NotFoundView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -29,6 +30,8 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import("/views/ExampleBootstrap.vue"),
     },
+            // default redirect to home page
+            { path: '/:pathMatch(.*)*', name: 'notfound', component: NotFoundView}
   ],
 });
 
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,14 @@
+<script >
+
+export default {
+  name: 'NotFoundView',
+
+}
+</script>
+
+<template>
+  <div>
+    <h1>Site was not found</h1>
+    <p>The URL could not be found. Try starting from the <router-link to="/">home page</router-link> again. </p>
+  </div>
+</template>

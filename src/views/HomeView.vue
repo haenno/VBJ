0199fe08151faf82db6148ee30268377aa2578ec
Henning 'haenno' Beier <haenno@web.de>
2023-08-13 21:56:59 +0200

    cleanup home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,11 @@
 <script setup>
-// example of using a store: increment a counter
-import { useCounterStore } from "@/store/counter.js";
-const counter = useCounterStore();
-counter.increment();
+
 </script>
 
 <template>
-  <div>Current count from pinia store: {{ counter.count }}</div>
+  
   <div>
-    <h1>This is an home page</h1>
+    <h1>Home...</h1>
   </div>
   <!-- Access the state directly from the store -->
 </template>
